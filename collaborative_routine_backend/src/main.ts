8557f7dc3f8c5605fee852288ec2275a32ea9442
Aneesh <as416106@gmail.com>
2025-12-17 23:16:15 +0530

    (feat) Auth API integration

--- collaborative_routine_backend/src/main.ts
+++ collaborative_routine_backend/src/main.ts
@@ -7,6 +7,11 @@ import { TransformInterceptor } from './common/interceptors/transform.intercepto
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173','http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
   const config = new DocumentBuilder()
     .setTitle('Collaborative Routine API')
     .setDescription(
@@ -22,14 +27,15 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('docs', app, documentFactory);
+  app.setGlobalPrefix('api');
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
   Logger.log(
-    `🚀 Server running at http://localhost:${port}`,
+    `🚀 Server running at http://localhost:${port}/api`,
     'NestApplication',
   );
-  Logger.log(`📚 Swagger available at http://localhost:${port}/api`, 'Swagger');
+  Logger.log(`📚 Swagger available at http://localhost:${port}/docs`, 'Swagger');
 }
 bootstrap();
